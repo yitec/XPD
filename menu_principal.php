0fe9642653e6b2d44046215707f50d199fa348bf
Sergio Barrantes <sergiob@thehangar.cr>
2012-12-27 09:30:51 -0600

    Update de interfaz

---
+++ menu_principal.php
@@ -0,0 +1,28 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"
+"http://www.w3.org/TR/html4/loose.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+        <link rel ="stylesheet" href="css/general.css" type="text/css" />
+        <link href='http://fonts.googleapis.com/css?family=Yanone+Kaffeesatz' rel='stylesheet' type='text/css' />
+        <title>Menu Principal</title>
+    </head>
+    <body >
+    		<h1>Salir</h1>        	    	
+    		<div class="box_mprincipal">
+                <div id="centrado_menu">
+        	       <div class="navi1">
+                        <ul>
+                        <li class="sm1"><a href="agenda.html"></a></li>
+                        <li class="sm2"><a href="URL_enlace2"></a></li>
+                        <li class="sm3"><a href="URL_enlace3"></a></li>
+                        <li class="sm4"><a href="URL_enlace4"></a></li>
+                        <li class="sm5"><a href="URL_enlace5"></a></li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="sub_footer">Seleccione una opción</div>                
+			</div>		
+    </body>
+</html>
+
