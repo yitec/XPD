f246644a927da06e01651e744b859f1308df166c
Sergio <sbarrantes@yitec.net>
2013-02-08 17:41:50 -0600

    upload

--- expedientes.php
+++ expedientes.php
@@ -11,9 +11,10 @@ conectar();
         <link rel ="stylesheet" href="css/general.css" type="text/css" />
         <link rel ="stylesheet" href="css/expedientes.css" type="text/css" />
         <link rel="stylesheet" href="http://code.jquery.com/ui/1.10.0/themes/base/jquery-ui.css" />
-        <link href='http://fonts.googleapis.com/css?family=Open+Sans+Condensed:300' rel='stylesheet' type='text/css' />
+        <link rel='stylesheet' href='http://fonts.googleapis.com/css?family=Open+Sans+Condensed:300' type='text/css' />
         <link rel="stylesheet" href="includes/themes/base/jquery-ui-1.10.0.custom.css" />
-        <link href="css/jquery.pnotify.default.css" rel="stylesheet" type="text/css" />
+        <link rel="stylesheet" href="css/jquery.pnotify.default.css" type="text/css" />
+
         <title>Expedientes</title>
     </head>
     <body >
@@ -22,7 +23,8 @@ conectar();
     		<div class="box_mprincipal">
                 <div class="sub_header">
                     <span id="add"><img src="img/add.png" id="boton_add" title="Crear un expediente."></span>
-                    <span id="search"><label for="buscar">Buscar:</label>&nbsp;&nbsp;&nbsp;&nbsp;<input type="text" class="inputbox" name="txt_buscar" title="Buscar un expediente." value="" /></span>                    
+                    <span id="boton_subir"><input  value="Subir Archivo" name="submit" id="upload_button" class="subir" />               </span>
+                    <span id="search"><label for="buscar">Buscar:</label>&nbsp;&nbsp;<input type="text" class="inputbox" name="txt_buscar" title="Buscar un expediente." value="" /></span>                    
                     <span id="search_icon"><img  src="img/search.png"></span>                    
                 </div>                
                     <div class="box_contenidos">   
@@ -54,8 +56,7 @@ conectar();
                     <tr>                        
                         <td>Acta aclaratoria de morosidad judicial</td>
                         <td>2-8-2010</td>
-                        <td>2-8-2012</td>
-                        <td ><img src="img/edit_icon.png" title="Editar"><img  class="iconos" src="img/download_icon.png" title="Descargar"><img  class="iconos" src="img/delete_icon.png" title="Eliminar"></td>                       
+                        <td>2-8-2012</                        <td ><img src="img/edit_icon.png" title="Editar"><img  class="iconos" src="img/download_icon.png" title="Descargar"><img  class="iconos" src="img/delete_icon.png" title="Eliminar"></td>                       
                     </tr>
                     <tr>                        
                         <td>Resolución Judicial Final de conciliación</td>
@@ -77,9 +78,11 @@ conectar();
                     </tr>
                     </table>            
                     </div>
-                    <div id="contenido">
+                    <div id="contenido">                        
                     </div>    
-
+                    <ul id="lista">
+</ul>
+                    
              
                 <div class="sub_footer">Seleccione una opción</div>                
 			</div>
@@ -106,10 +109,18 @@ conectar();
   </form>
 </div>
 
+<div id="dialog-form-subir" title="Subir un archivo">
+  <p class="validateTips">Seleccione el archivo.</p>
+  
+  
+  
+</div>
+
     </body>
-<script type="text/javascript" src="includes/jquery-1.9.0.min.js"></script>   
+<script src="includes/jquery-1.9.0.min.js"type="text/javascript" ></script>   
 <script src="includes/ui/jquery-ui.js"></script> 
-<script type="text/javascript" src="includes/Scripts_Expedientes.js"></script>
+<script src="includes/Scripts_Expedientes.js"type="text/javascript" ></script>
 <script src="includes/jquery.pnotify.js" type="text/javascript"></script> 
+<script src="includes/ajaxupload.js"language="javascript"></script>
 </html>
 
