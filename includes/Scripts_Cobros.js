d08e5e0224657c95cdd675d18191b11032aea3a5
Sergio <sbarrantes@yitec.net>
2013-03-15 17:28:52 -0600

    Solucionado espacion en blanco

--- includes/Scripts_Cobros.js
+++ includes/Scripts_Cobros.js
@@ -329,13 +329,55 @@ $( document ).tooltip({
 
 /**********************************Poner separadores de miles******************************************/
 
+
 $('#txt_montom').keydown(function(){
-    var str = $(this).val();
-    str = str.replace(/\D+/g, '');
-    $(this).val(str.replace(/\d(?=(?:\d{3})+(?!\d))/g, '$& '));
+    pat = /[\*,\+,\(,\),\?,\,$,\[,\],\^]/;
+  valor = donde.value;
+  largo = valor.length;
+  crtr = true;
+  pat = /[\*,\+,\(,\),\?,\,$,\[,\],\^]/;
+  valor = donde.value;
+  largo = valor.length;
+  crtr = true;
+  if(isNaN(caracter) || pat.test(caracter) == true){
+    if (pat.test(caracter)==true){ 
+      caracter = "\" + caracter
+    }
+    carcter = new RegExp(caracter,"g")
+    valor = valor.replace(carcter,"")
+    donde.value = valor
+    crtr = false
+  }
+  else{
+    var nums = new Array()
+    cont = 0
+    for(m=0;m<largo;m++){
+      if(valor.charAt(m) == "." || valor.charAt(m) == " ")
+        {continue;}
+      else{
+        nums[cont] = valor.charAt(m)
+        cont++
+      }
+    }
+  }
+  var cad1="",cad2="",tres=0
+  if(largo > 3 && crtr == true){
+    for (k=nums.length-1;k>=0;k--){
+      cad1 = nums[k]
+      cad2 = cad1 + cad2
+      tres++
+      if((tres%3) == 0){
+        if(k!=0){
+          cad2 = "." + cad2
+        }
+      }
+    }
+    donde.value = cad2
+  }
+
 });
 
-/************************************Notificaciones Jquery************************************************************/
+/************************************Notificaciones Jquery*********************************************/
 function notificacion(titulo,cuerpo,tipo){
   $.pnotify({
   pnotify_title: titulo,
@@ -344,17 +386,22 @@ function notificacion(titulo,cuerpo,tipo){
     pnotify_hide: true
   }); 
 }
-
+/***********************************Elimina espacios en blanco de monto********************************/
 function fTrim(Str)
 {
-  cadena = "" + Str + "";
   
-  for(i=0; i=0; i=cadena.length-1)
+  var tot=Str.length-1;
+  var cadena ="";
+  var letra=""
+  for(i=0; i<=tot; i++)
   {
-    if(cadena.charAt(i)==" ")
-      cadena=cadena.substring(0,i);
-    else
-      break;
+    if(Str.charAt(i)==" "){
+      i++;
+      cadena=cadena+Str.charAt(i);
+    }else{
+      cadena=cadena+Str.charAt(i);
+    }
+    
   }
   
   return cadena;
